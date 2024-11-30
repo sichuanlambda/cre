03907cafe81def76e9aef66c465331463f625ebc
sichuanlambda <cdnathan.robinson@gmail.com>
2024-11-30 12:58:59 +0100

    basic files & updates for fetching website info

--- app/controllers/analyzer_controller.rb
+++ app/controllers/analyzer_controller.rb
@@ -1,13 +1,30 @@
+require 'net/http'
+require 'uri'
+
 class AnalyzerController < ApplicationController
   def new
   end
 
   def analyze
     @website = params[:website]
+    @website = "https://#{@website}" unless @website.start_with?('http://', 'https://')
 
-    service = OpenAiService.new(@website)
-    @result = service.analyze
+    begin
+      uri = URI.parse(@website)
+      response = Net::HTTP.get_response(uri)
 
-    render :new
+      @analysis = {
+        status: response.code,
+        content_length: response.body.length,
+        title: response.body.match(/<title>(.*?)<\/title>/i)&.captures&.first,
+        description: response.body.match(/<meta\s+name="description"\s+content="(.*?)"/i)&.captures&.first
+      }
+    rescue => e
+      @error = "Error analyzing website: #{e.message}"
+    end
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 end
